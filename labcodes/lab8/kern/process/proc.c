edd2e63baf16c4d522a737205a5d91af1b9094ac
oyqy <oyqy15@mails.tsinghua.edu.cn>
2018-05-21 22:10:35 +0800

    lab8ex0

--- labcodes/lab8/kern/process/proc.c
+++ labcodes/lab8/kern/process/proc.c
@@ -106,6 +106,23 @@ alloc_proc(void) {
      *       uint32_t flags;                             // Process flag
      *       char name[PROC_NAME_LEN + 1];               // Process name
      */
+      proc->state = PROC_UNINIT;
+      proc->pid = -1;
+      proc->cr3 = boot_cr3;
+
+      proc->runs = 0;
+      proc->kstack = 0;
+      proc->need_resched = 0;
+      proc->parent = NULL;
+      proc->mm = NULL;
+      memset(&(proc->context) , 0 , sizeof(proc->context));
+      proc->tf = NULL;
+      proc->flags = 0;
+      memset(proc->name, 0 ,sizeof(proc->name));
+
+      proc->exit_code = 0;
+      proc->cptr = proc->yptr = proc->optr = NULL;
+      proc->wait_state =0;
      //LAB5 YOUR CODE : (update LAB4 steps)
     /*
      * below fields(add in LAB5) in proc_struct need to be initialized	
@@ -122,6 +139,14 @@ alloc_proc(void) {
      *     uint32_t lab6_stride;                       // FOR LAB6 ONLY: the current stride of the process
      *     uint32_t lab6_priority;                     // FOR LAB6 ONLY: the priority of process, set by lab6_set_priority(uint32_t)
      */
+
+      proc->rq = NULL;
+      proc->time_slice = 0;
+      proc->lab6_run_pool.left = proc->lab6_run_pool.right = proc->lab6_run_pool.parent = NULL;
+      proc->lab6_stride = 0;
+      proc->lab6_priority = 0;
+
+
     //LAB8:EXERCISE2 YOUR CODE HINT:need add some code to init fs in proc_struct, ...
     }
     return proc;
@@ -427,6 +452,28 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
         goto fork_out;
     }
     ret = -E_NO_MEM;
+
+    proc = alloc_proc();
+    if (proc == NULL) goto fork_out;
+    proc->parent = current;
+    assert(current->wait_state == 0);
+
+    if (setup_kstack(proc)!=0) goto bad_fork_cleanup_kstack;
+    if (copy_mm(clone_flags, proc)!=0) goto bad_fork_cleanup_proc;
+    copy_thread(proc, stack, tf);
+
+    bool intr_flag;
+    local_intr_save(intr_flag); //according to answer
+
+    proc->pid = get_pid();
+    hash_proc(proc);
+    set_links(proc);
+    wakeup_proc(proc);
+
+    local_intr_restore(intr_flag); //according to answer
+
+    ret = proc->pid;
+
     //LAB4:EXERCISE2 YOUR CODE
     //LAB8:EXERCISE2 YOUR CODE  HINT:how to copy the fs in parent's proc_struct?
     /*
